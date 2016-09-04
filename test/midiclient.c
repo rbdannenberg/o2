7500cf87208ace5d5ab3fd9b04a68e25e713ca1c
Roger B. Dannenberg <rbd@cs.cmu.edu>
2016-09-03 22:44:49 -0400

    Added midiclient and midiserver for testing on OS X.

---
+++ test/midiclient.c
@@ -0,0 +1,58 @@
+// midiclient.c - an example program, send to midiserver
+//
+// This program works with midiserver.c.
+
+#include "o2.h"
+#include "stdio.h"
+#include "string.h"
+#include "cmtio.h"
+
+#ifdef WIN32
+#include <windows.h> 
+#else
+#include <unistd.h>
+#endif
+
+#pragma comment(lib,"o2_static.lib")
+
+#define N_ADDRS 20
+
+char *server_addresses[N_ADDRS];
+int msg_count = 0;
+
+
+int main(int argc, const char * argv[])
+{
+    /* establish non-blocking input so we can "type" some notes */
+    IOsetup(0); // inputfd: 0 means stdin
+
+    o2_initialize("miditest");
+    
+    while (o2_status("midi") < O2_LOCAL) {
+        o2_poll();
+        usleep(2000); // 2ms
+    }
+    printf("We discovered the midi service.\ntime is %g.\n", o2_get_time());
+    
+    printf("Here we go! ...\ntime is %g.\n", o2_get_time());
+    
+    while (1) {
+        o2_poll();
+        int input = IOgetchar();
+        if (input > 0) {
+            char *keys = "qwertyuiopasdfghjklzxcvbnm";
+            char *keyloc = strchr(keys, input);
+            if (keyloc) {
+                input = (keyloc - keys) + 48; // index of found key to pitch
+                double now = o2_get_time();
+                o2_send_cmd("/midi/midi", 0.0, "iii", 0x90, input, 127);
+                o2_send_cmd("/midi/midi", now + 1.0, "iii", 0x90, input, 0);
+                printf("sent key number %d at %g\n", input, now);
+            }
+        }
+        usleep(2000); // 2ms
+    }
+
+    o2_finish();
+    return 0;
+}
---
+++ test/midiserver.c
@@ -0,0 +1,71 @@
+//  midiserver.c - example program, receive O2, send MIDI
+//
+//  This program works with midiclient.c. 
+//
+
+#include "o2.h"
+#include "stdio.h"
+#include "string.h"
+#include "portmidi.h"
+#include "porttime.h" // maybe not needed
+
+#define TIME_PROC ((int32_t (*)(void *)) Pt_Time)
+#define TIME_INFO NULL
+
+PmStream *midi_out = NULL; // MIDI output stream
+
+#ifdef WIN32
+#include <windows.h> 
+#else
+#include <unistd.h>
+#endif
+
+#pragma comment(lib,"o2_static.lib")
+
+// this is a handler for incoming messages. It simply builds a
+// MIDI message and sends it using portmidi
+//
+int midi_handler(o2_message_ptr msg, const char *types,
+                 o2_arg ** argv, int argc, void *user_data)
+{
+    int status = argv[0]->i32;
+    int data1 = argv[1]->i32;
+    int data2 = argv[2]->i32;
+    Pm_WriteShort(midi_out, 0, Pm_Message(status, data1, data2));
+    printf("Pm_WriteShort(%2x %2x %2x at %g\n", status, data1, data2, o2_get_time());
+    return O2_SUCCESS;
+}
+
+
+int main(int argc, const char * argv[])
+{
+    // start portmidi
+    Pt_Start(1, 0, 0);
+
+    int dev_num = Pm_GetDefaultOutputDeviceID();
+    printf("Using default PortMidi ouput device number %d\n", dev_num);
+
+    Pm_OpenOutput(&midi_out, dev_num, NULL, 0, TIME_PROC, TIME_INFO, 0);
+
+    o2_initialize("miditest"); // ideally, this application name should be
+    // passed from the command line so we provide service to any application
+    o2_add_service("midi");
+    
+    // add our handler for incoming messages to each server address
+    o2_add_method("/midi/midi", "iii", &midi_handler, NULL, TRUE, TRUE);
+    
+    // we are the master clock
+    o2_set_clock(NULL, NULL);
+    
+    printf("Here we go! ...\ntime is %g.\n", o2_get_time());
+    
+    while (TRUE) {
+        o2_poll();
+        usleep(1000); // 1ms
+    }
+
+    o2_finish();
+    Pm_Close(midi_out);
+    Pm_Terminate();
+    return 0;
+}
