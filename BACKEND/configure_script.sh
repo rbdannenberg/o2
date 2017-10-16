8750af5d3b6f5ed54d45337ac4d4201293d0774f
aparrnar <aparrnar@andrew.cmu.edu>
2017-10-16 00:17:28 -0400

    Made changes to clone and build O2 once per test run, consolidate test outputs with timestamped folder

--- BACKEND/configure_script.sh
+++ BACKEND/configure_script.sh
@@ -7,22 +7,29 @@ ip="$1"
 os="$2"
 password="$3"
 username="$4"
-testcase="$5"
+testcases="$5"
 hostname="aparrnaa"
-hostip="128.237.194.69"
+hostip="10.0.0.230"
 echo "executing ssh..."
+datevar=`date +%Y-%m-%d-%H-%M-%S`
 HOMEPATH="/home/osboxes/o2"
-HOMEPATH="$HOMEPATH/$testcase.txt"
+test=(${testcases//,/ })
+
 echo $HOMEPATH
-/usr/local/bin/sshpass -p $3 ssh -t -t -o StrictHostKeyChecking=no $username@$ip /home/osboxes/build_script.sh $os $testcase
+echo $datevar
+/usr/local/bin/sshpass -p $3 ssh -t -t -o StrictHostKeyChecking=no $username@$ip /home/osboxes/build_script.sh $os $testcases
 echo "Done.."
-if [ ! -d "$HOMEDIR/$ip" ];
+if [ ! -d "$HOMEDIR/$ip/$datevar" ];
 then
-mkdir $HOMEDIR/$ip
+mkdir $HOMEDIR/$ip/$datevar
 else
-  cd $HOMEDIR/$ip
+  cd $HOMEDIR/$ip/$datevar
 fi
-/usr/local/bin/sshpass -p $3 ssh -t -t -o StrictHostKeyChecking=no $username@$ip <<EOF
-sshpass -p "123" scp $HOMEPATH $hostname@$hostip:$HOMEDIR/$ip/
-logout
+for element in "${test[@]}"
+do
+   echo "$element"
+   /usr/local/bin/sshpass -p $3 ssh -t -t -o StrictHostKeyChecking=no $username@$ip <<EOF
+   sshpass -p "123" scp $HOMEPATH/$element.txt $hostname@$hostip:$HOMEDIR/$ip/$datevar
+   logout
 EOF
+done
