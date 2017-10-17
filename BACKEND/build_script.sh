7af398a51f6109a3f074e21534daf132443e3a2c
aparrnar <aparrnar@andrew.cmu.edu>
2017-10-16 23:41:00 -0400

    Changes to cloning and building o2 once per test run

--- BACKEND/build_script.sh
+++ BACKEND/build_script.sh
@@ -52,17 +52,23 @@ make
 fi
 wait
 echo "Executing test case.."
-echo $2
-if [ -f "$2" ];
-then
-./$2 > $DIRECTORY/$2.txt
-if grep -Fxq "DONE" $DIRECTORY/$2.txt
-    then
-        echo "PASS"
+testcases="$2"
+test=(${testcases//,/ })
+for element in "${test[@]}"
+do
+	echo $element
+	   if [ -f $element ];
+	then
+	./$element > $DIRECTORY/$element.txt
+	if grep -Fxq "DONE" $DIRECTORY/$element.txt
+	    then
+		echo "PASS"
+	fi
+	echo "Output successful.."
+	cat $DIRECTORY/$element.txt
+	else
+	echo "Invalid test case input, Please enter valid test case name.."
+	fi
+done
 fi
-echo "Output successful.."
-cat $DIRECTORY/$2.txt
-else
-echo "Invalid test case input, Please enter valid test case name.."
-fi
-fi
+
