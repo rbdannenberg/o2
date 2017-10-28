2a676af63bf99074b6775c55017cb1ea257d2d37
aparrnar <aparrnar@andrew.cmu.edu>
2017-10-28 01:30:34 -0400

    UI Changes Latest Commit

--- Front_end-Files/O2_GUI.java
+++ Front_end-Files/O2_GUI.java
@@ -14,22 +14,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableColumn;
+import sun.security.util.Password;
 
 @SuppressWarnings("serial")
 public class O2_GUI extends JPanel implements ActionListener{
 
 final JTable table = new JTable(new MyTableModel());
-JTextArea output;
-private Set<String> contents = new HashSet();
 
+JTextArea output;
+JPasswordField password;
+private Set<String> contents = new HashSet();
+public String[] myMachineDetails;
 public O2_GUI() {
     initializePanel();
 }
@@ -38,14 +45,13 @@ private void initializePanel() {
     setLayout(new BorderLayout());
     setPreferredSize(new Dimension(600, 600));
     setName("Configure Machines");
-
-
+    JLabel passwordlabel = new JLabel("Enter the password for the local machine: ");
+    password = new JPasswordField(20);
     table.setFillsViewportHeight(true);
     JScrollPane pane = new JScrollPane(table);
     JButton add = new JButton("Configure");
     add.addActionListener(this);
-    JButton add1 = new JButton("Logs");
-
+   // JButton add1 = new JButton("Logs");
     table.setRowSelectionAllowed(true);
     table.setColumnSelectionAllowed(false);
     table.getSelectionModel().addListSelectionListener(new SharedListSelectionHandler());
@@ -58,20 +64,77 @@ private void initializePanel() {
  
     JPanel command = new JPanel(new FlowLayout());
     output = new JTextArea(1, 10);
+    output.setVisible(false);
     command.add(add);
-    command.add(add1);
+   // command.add(add1);
     command.add(output);
-
+    JPanel first = new JPanel(new FlowLayout());
+    first.add(passwordlabel);
+    first.add(password);
+    //add(passwordlabel, BorderLayout.PAGE_START); 
+   // add(password, BorderLayout.LINE_END);
     add(pane, BorderLayout.CENTER);
     add(command, BorderLayout.SOUTH);
+    add(first, BorderLayout.PAGE_START);
     
+    myMachineDetails = new String[4];
+    String interfaceName;
+    String osname = System.getProperty("os.name").toLowerCase();
+    if(osname.indexOf("win") > 0) 
+    {
+        interfaceName = "eth0";
+        osname = "windows";
+    }
+    else if(osname.indexOf("x") > 0) 
+    {
+        interfaceName = "en0";
+        osname = "mac";
+    }
+    else
+    {
+        interfaceName = "en0";
+        osname = "ubuntu";
+    }
+   
+    String ip = null;
+    try
+    {
+         NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
+         Enumeration<InetAddress> inetAddress = networkInterface.getInetAddresses();
+         InetAddress currentAddress;
+         currentAddress = inetAddress.nextElement();
+         while(inetAddress.hasMoreElements())
+         {
+             currentAddress = inetAddress.nextElement();
+             if(currentAddress instanceof Inet4Address && !currentAddress.isLoopbackAddress())
+             {
+                  ip = currentAddress.toString();
+                 //System.out.println(ip.substring(1));
+                 break;
+             }
+         } 
+    }
+    catch(Exception e)
+    {
+        e.getMessage();
+    } 
+
+    String username = System.getProperty("user.name");
+    myMachineDetails[0] = ip.substring(1);
+    myMachineDetails[1] = osname;
+    myMachineDetails[2] = username;
+    myMachineDetails[3] = new String(password.getPassword());
+   // System.out.println();
  // add a nice border
     setBorder(new EmptyBorder(5, 5, 5, 5));
     CSVFile Rd = new CSVFile();
     MyTableModel NewModel = new MyTableModel();
     this.table.setModel(NewModel);
     File DataFile = new File("/Users/aparrnaa/NetBeansProjects/UI2/sample.csv");
-    ArrayList<String[]> Rs2 = Rd.ReadCSVfile(DataFile);
+    ArrayList<String[]> Rs2 = new ArrayList < String[]>();
+   
+    Rs2 = Rd.ReadCSVfile(DataFile);
+    Rs2.add(myMachineDetails);
     NewModel.AddCSVData(Rs2);
     System.out.println("Rows: " + NewModel.getRowCount());
     System.out.println("Cols: " + NewModel.getColumnCount());
@@ -87,74 +150,14 @@ private void initializePanel() {
         
         ArrayList <String> machines = new ArrayList(Arrays.asList(machineList));
     try {
-        GU.O2_GUI_2_Testcases(machines);
+       myMachineDetails[3]= new String(password.getPassword());
+        GU.O2_GUI_2_Testcases(machines, myMachineDetails);
     } catch (IOException ex) {
         Logger.getLogger(O2_GUI.class.getName()).log(Level.SEVERE, null, ex);
     }
    
         GU.setVisible(true);
-       //  System.out.println("ip"+machineDetails[0]);
-        //System.out.println("os:"+machineDetails[1]);
-        //System.out.println("username:"+machineDetails[2]);
-       // System.out.println("password:"+machineDetails[3]);
-        /*try {
-        String line;
-        Process p = Runtime.getRuntime().exec("/Users/aparrnaa/Desktop/CMU/Practicum/o2_MAIN_COPY/BACKEND/configure_script.sh"+" "+machineDetails[0]+","+machineDetails[1]+","+machineDetails[2]+","+machineDetails[3]);
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(p.getInputStream()) );
-        
-        while ((line = in.readLine()) != null) {
-        System.out.println(line);
-        }
-        in.close();
-        
-        BufferedReader err = new BufferedReader(
-        new InputStreamReader(p.getErrorStream()) );
-        
-        while ((line = err.readLine()) != null) {
-        System.out.println(line);
-        }
-        OutputStream outputStream = p.getOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        printStream.println();
-        printStream.flush();
-        printStream.close();
-
-        }
-        catch (IOException ex) {
-        Logger.getLogger(O2_GUI.class.getName()).log(Level.SEVERE, null, ex);
-        } */
-     /*   String[] command = {"/Users/aparrnaa/Desktop/CMU/Practicum/o2_MAIN_COPY/BACKEND/configure_script.sh", machineDetails[0], machineDetails[1], machineDetails[3], machineDetails[2]};
-        ProcessBuilder p = new ProcessBuilder(command);
-        Process p2 = null;
-        try {
-            p2 = p.start();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-            Logger.getLogger(O2_GUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        BufferedReader br = new BufferedReader(new InputStreamReader(p2.getInputStream()));
-        String line;
-        p.redirectErrorStream(true);
-        System.out.println("Output of running " + command + " is: ");
-        try {
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(O2_GUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
- 
-        
-       System.out.println("Error in running " + command + " is: ");
-        try {
-        BufferedReader br2 = new BufferedReader(new InputStreamReader(p2.getErrorStream()));
-        while ((line = br2.readLine()) != null) {
-        System.out.println(line);
-        }
-        } catch (IOException ex) {
-        Logger.getLogger(O2_GUI.class.getName()).log(Level.SEVERE, null, ex);
-        }  */
+       
     } 
 
 
