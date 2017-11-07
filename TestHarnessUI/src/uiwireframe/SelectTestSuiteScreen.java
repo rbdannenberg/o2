135718f7d4c207c6961a837fa8c982f3be0b564c
aparrnar <aparrnar@andrew.cmu.edu>
2017-11-07 02:02:06 -0500

    create test suite screen basic version pushed

--- TestHarnessUI/src/uiwireframe/SelectTestSuiteScreen.java
+++ TestHarnessUI/src/uiwireframe/SelectTestSuiteScreen.java
@@ -131,6 +131,7 @@ public class SelectTestSuiteScreen extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
         CreateTestSuiteScreen nextScreen = new CreateTestSuiteScreen();
+        //nextScreen.getContent();
         nextScreen.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton4ActionPerformed
