63418925905410bdc32da0d947d13bd71cc23446
rbdannenberg <rbd@cs.cmu.edu>
2021-01-07 16:26:58 -0500

    missing file added to repo

---
+++ src/hostip.h
@@ -0,0 +1,17 @@
+// room for IP address in dot notation and terminating EOS
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define O2N_IP_LEN 16
+
+extern bool o2n_network_enabled;
+extern bool o2n_network_found;
+extern char o2n_internal_ip[O2N_IP_LEN];
+
+void o2n_get_internal_ip(void);
+void o2_hex_to_dot(const char *hex, char *dot);
+
+#ifdef __cplusplus
+}
+#endif
