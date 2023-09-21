7ac9e6efb6a50c63370dcaf84e779e63173f30be
ruaruarua <shen.zek@northeastern.edu>
2023-09-21 15:23:17 -0400

    1-2 create classes and basic structure.

--- o2litepy/o2litedisc.py
+++ o2litepy/o2litedisc.py
@@ -2,3 +2,11 @@ class O2liteDisc:
     def __init__(self):
         pass
 
+    def o2ldisc_init(self):
+        pass
+
+    def o2ldisc_poll(self):
+        pass
+
+    def o2ldisc_events(self):
+        pass
